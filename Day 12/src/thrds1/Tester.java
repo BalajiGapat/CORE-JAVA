fab65d1a103de2e952c4f39692d7f3ad02e1c5a2
Balaji Gapat <gapatbalaji2901@gmail.com>
2023-06-28 22:43:19 +0530

    java day 12

---
+++ Day 12/src/thrds1/Tester.java
@@ -0,0 +1,27 @@
+package thrds1;
+import static java.lang.Thread.*;
+
+public class Tester 
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.out.println("main thrd detail : "+ Thread.currentThread());// Thread [main,5,main]
+		// create child thrds
+		MyThread t1 = new MyThread("one");
+		MyThread t2 = new MyThread("two");
+		MyThread t3 = new MyThread("three");
+		MyThread t4 = new MyThread("four");
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();// RUNNABLE : 1(parent)+4(child)
+		for (int i = 11; i < 20; i++) {
+			System.out.println(currentThread().getName() + " exec # " + i);
+			sleep(1000);
+		}
+		System.out.println("main over......");
+
+	}
+
+}
